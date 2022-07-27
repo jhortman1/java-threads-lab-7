7648305eff1fcb3ae6605c3e2800d5859ad9e7a0
James Hortman <jamesdhortman@gmail.com>
2022-07-27 11:08:13 -0700

    threads lab 7 complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 import java.util.concurrent.Callable;
 public class Main {
-    public static void Main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         Integer numCallable = getCallableInteger().call();
-        System.out.print(numCallable);
+        System.out.printf("Callable Number: %s",numCallable);
     }
     public static Callable<Integer> getCallableInteger() {
         // your code here
         Scanner scanner = new Scanner(System.in);
         Callable<Integer> callableNum = ()-> scanner.nextInt();
-        scanner.close();
+
         return callableNum;
     }
 }
