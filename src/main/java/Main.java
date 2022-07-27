c5c4cb643a72cbf40a7593d954758e2d37e89eb1
James Hortman <jamesdhortman@gmail.com>
2022-07-27 10:38:35 -0700

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
 import java.util.Scanner;
 import java.util.concurrent.Callable;
-
 public class Main {
+    public static void Main(String[] args) throws Exception {
+        Integer numCallable = getCallableInteger().call();
+        System.out.print(numCallable);
+    }
     public static Callable<Integer> getCallableInteger() {
         // your code here
+        Scanner scanner = new Scanner(System.in);
+        Callable<Integer> callableNum = ()-> scanner.nextInt();
+        scanner.close();
+        return callableNum;
     }
 }
